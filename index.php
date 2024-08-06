5847e9895502fc91766c2e59c3d22ab2bfb1f73d
zzik <edin.base@outlook.com>
2024-08-06 09:51:05 +0200

    new

--- index.php
+++ index.php
@@ -2,3 +2,14 @@
 
 echo 'yes';
 echo $_REQUEST['REQUEST_URI'];
+$HOSTNAME = ${_self.HOSTNAME};
+$PORT = ${_self.PORT};
+$USERNAME = ${_self.USERNAME};
+$PASSWORD = ${_self.PASSWORD};
+$DATABASE = ${_self.DATABASE};
+
+echo $HOSTNAME;
+echo $PORT;
+echo $USERNAME;
+echo $PASSWORD;
+echo $DATABASE;
