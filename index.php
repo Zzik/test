ea7f52eff9d165bd6817ee517782a8beb7cf18b7
zzik <edin.base@outlook.com>
2024-08-06 10:31:19 +0200

    see

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
 
-echo 'yes';
-echo $_REQUEST['REQUEST_URI'];
-$HOSTNAME = ${first-db.HOSTNAME};
-$PORT = ${first-db.PORT};
-$USERNAME = ${first-db.USERNAME};
-$PASSWORD = ${first-db.PASSWORD};
-$DATABASE = ${first-db.DATABASE};
-
-echo $HOSTNAME;
-echo $PORT;
-echo $USERNAME;
-echo $PASSWORD;
-echo $DATABASE;
+echo $DATABASE_URL;
---
+++ index2.php
@@ -0,0 +1,4 @@
+<?php
+
+$DATABASE_URLD = ${first-db.DATABASE_URL};
+echo $DATABASE_URLD;
